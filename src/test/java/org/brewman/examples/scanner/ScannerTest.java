1fddf7619c31b6d13c293ab4ee311e9bd8d87218
Daniel Shiplett <daniel.shiplett@g2-inc.com>
2015-05-29 14:36:28 -0400

    Update to use a differnt input to the Scanner.

--- src/test/java/org/brewman/examples/scanner/ScannerTest.java
+++ src/test/java/org/brewman/examples/scanner/ScannerTest.java
@@ -1,5 +1,8 @@
 package org.brewman.examples.scanner;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -17,8 +20,33 @@ public class ScannerTest {
     private static final int STEP_TIMEOUT = 10;
 
     @Test
-    public void test() {
-        readLine(new Scanner(System.in));
+    public void test() throws IOException {
+        String data = "Users Input";
+
+        System.setIn(new InputStream() {
+            ByteArrayInputStream bos = new ByteArrayInputStream(data.getBytes());
+            boolean first = true;
+
+            @Override
+            public int read() throws IOException {
+                if (first) {
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+                        LOG.error(e.getMessage());
+                    }
+                    first = false;
+                }
+
+                int rtn = bos.read();
+                LOG.debug("char: {}", rtn);
+                return rtn;
+            }
+        });
+
+        Scanner scanner = new Scanner(System.in);
+
+        readLine(scanner);
     }
 
     public static String readLine(Scanner scanner) {
